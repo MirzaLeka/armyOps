056fb05cb3ebc0b823080eb8e065c2fcc1d0af4b
Mirza Leka <mirza_376@hotmail.com>
2018-10-31 19:24:41 +0100

    Added modal - image preview

--- Resources/js/scripts.js
+++ Resources/js/scripts.js
@@ -5,10 +5,6 @@ getMedia();
 
 }
 
-function getMedia() {
-
-let media = document.querySelector(".media");
-
 let items = [
     "../Resources/img/media/1.png",
     "../Resources/img/media/2.jpg",
@@ -29,12 +25,25 @@ let items = [
     "../Resources/img/media/15.png",
 ]
 
+function getMedia() {
+
+// let media = document.querySelector(".media");
+
+let item = '';
+
 for (let i = 0; i < items.length; i++) {
-    let mediaItem = document.createElement("img"); 
-    mediaItem.setAttribute("src", items[i]);
-    mediaItem.classList = "mediaItems";
-    media.appendChild(mediaItem);
+    item += `<img class="mediaItems" onclick="openModal(${i})" src="${items[i]}"/>`;
 }
+
+$(".media").append(item);
+
+// for (let i = 0; i < items.length; i++) {
+//     let mediaItem = document.createElement("img"); 
+//     mediaItem.addEventListener("click", openModal(i));
+//     mediaItem.setAttribute("src", items[i]);
+//     mediaItem.classList = "mediaItems";
+//     media.appendChild(mediaItem);
+// }
 
 // for (let i of items) {
 // let mediaItem = document.createElement("img");
@@ -45,3 +54,65 @@ for (let i = 0; i < items.length; i++) {
 //     }
 
 }
+
+
+let globalSrc = 0;
+let changeCaption = 0;
+let modal =  document.querySelector("#myModal");
+
+// open modal
+
+function openModal(src) {
+
+    modal.style.display = "flex";
+
+    document.querySelector(".modalImg").setAttribute("src", items[src])
+
+   globalSrc = src;
+
+   }
+
+
+function nextImg() {
+    globalSrc++;
+
+    if (globalSrc == items.length) {
+        globalSrc = 1;
+    }
+
+    document.querySelector(".modalImg").setAttribute("src", items[globalSrc])    
+ 
+}
+
+function prevImg() {
+    globalSrc--;
+
+    if (globalSrc < 1) {
+        globalSrc = items.length-1;
+    }
+
+    document.querySelector(".modalImg").setAttribute("src", items[globalSrc])
+
+}
+
+  // Close modal when you press X
+  let closeModal = document.querySelectorAll(".closeModal");
+  
+  closeModal.forEach(item => {
+      item.addEventListener("click", () => {
+        modal.style.display = "none";
+        });
+  });
+
+
+//   $(".closeModal").click(function() {
+//     $("#myModal").fadeOut();
+//   });
+
+  // Close modal when you click anywhere on window
+
+  window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";    
+      }
+  }
